4812be49c7793a002536b13316298c867baac075
storm <storm@192.168.39.128>
2022-10-11 13:46:50 +0200

    2a unnagjort

--- src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
+++ src/no/hvl/dat100ptc/oppgave2/GPSDataConverter.java
@@ -8,22 +8,30 @@ public class GPSDataConverter {
 	// konverter tidsinformasjon i gps data punkt til antall sekunder fra midnatt
 	// dvs. ignorer information om dato og omregn tidspunkt til sekunder
 	// Eksempel - tidsinformasjon (som String): 2017-08-13T08:52:26.000Z
-    // skal omregnes til sekunder (som int): 8 * 60 * 60 + 52 * 60 + 26 
-	
+	// skal omregnes til sekunder (som int): 8 * 60 * 60 + 52 * 60 + 26
+
 	private static int TIME_STARTINDEX = 11; // posisjon for start av tidspunkt i timestr
 
 	public static int toSeconds(String timestr) {
-		
+		// Gjør String om til sekunder
+
 		int secs;
 		int hr, min, sec;
-		
-		// TODO
-		// OPPGAVE - START
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-		// OPPGAVE - SLUTT
-		
+		// 2017-08-13T08:52:26.000Z
+		String hr_S = timestr.substring(11, 13);
+		hr = Integer.parseInt(hr_S);
+
+		String min_S = timestr.substring(14, 16);
+		min = Integer.parseInt(min_S);
+
+		String sec_S = timestr.substring(17, 19);
+		sec = Integer.parseInt(sec_S);
+
+		secs = hr * 60 * 60 + min * 60 + sec;
+
+		return secs;
+
 	}
 
 	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
@@ -32,10 +40,20 @@ public class GPSDataConverter {
 
 		// TODO - START ;
 		
-		throw new UnsupportedOperationException(TODO.method());
+		int time = toSeconds(timeStr);
+		// 2017-08-13T08:52:26.000Z","60.385390","5.217217","61.9"
+		
+		double latitude = Double.parseDouble(latitudeStr);
+		
+		double longitude = Double.parseDouble(longitudeStr);
+		
+		double elevation = Double.parseDouble(elevationStr);
 
+		gpspoint = new GPSPoint(time, latitude, longitude, elevation);
+		
+		return gpspoint;
 		// OPPGAVE - SLUTT ;
-	    
+
 	}
-	
+
 }
