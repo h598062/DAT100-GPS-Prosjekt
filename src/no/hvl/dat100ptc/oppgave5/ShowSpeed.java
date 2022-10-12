508b25a21903c02fb8d23567a9f59d28d901752e
Spirevipp <bjornar.helgeland@hotmail.com>
2022-10-12 19:17:06 +0200

    :tada: Gjort oppgave5 b kanskje, kan ikke teste før averageSpeed funksjon i oppgave4 er ferdig

--- src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
@@ -11,22 +11,22 @@ import no.hvl.dat100ptc.oppgave3.GPSUtils;
 import no.hvl.dat100ptc.oppgave4.GPSComputer;
 
 public class ShowSpeed extends EasyGraphics {
-			
-	private static final int MARGIN = 50;
+
+	private static final int MARGIN    = 50;
 	private static final int BARHEIGHT = 200; // assume no speed above 200 km/t
 
 	private GPSComputer gpscomputer;
-	private GPSPoint[] gpspoints;
-	
+	private GPSPoint[]  gpspoints;
+
 	public ShowSpeed() {
 
 		String filename = JOptionPane.showInputDialog("GPS data filnavn: ");
 		gpscomputer = new GPSComputer(filename);
 
 		gpspoints = gpscomputer.getGPSPoints();
-		
+
 	}
-	
+
 	// read in the files and draw into using EasyGraphics
 	public static void main(String[] args) {
 		launch(args);
@@ -34,24 +34,29 @@ public class ShowSpeed extends EasyGraphics {
 
 	public void run() {
 
-		int N = gpspoints.length-1; // number of data points
-		
-		makeWindow("Speed profile", 2*MARGIN + 2 * N, 2 * MARGIN + BARHEIGHT);
-		
-		showSpeedProfile(MARGIN + BARHEIGHT,N);
+		int N = gpspoints.length - 1; // number of data points
+
+		makeWindow("Speed profile", 2 * MARGIN + 2 * N, 2 * MARGIN + BARHEIGHT);
+
+		showSpeedProfile(MARGIN + BARHEIGHT, N);
 	}
-	
+
 	public void showSpeedProfile(int ybase, int N) {
 
-		// get segments speeds from the GPS computer object		
-		double[] speeds = gpscomputer.speeds();
+		// get segments speeds from the GPS computer object
+		double[] speeds   = gpscomputer.speeds();
+		int      avgSpeed = (int) gpscomputer.averageSpeed();
+		setColor(0, 0, 255);
+		for (int i = 0; i < speeds.length; i++) {
+			int x = MARGIN + i * 3;
+			int y = (int) speeds[i];
+			if (y < 0) {
+				y = 0;
+			}
+			drawLine(x, ybase, x, ybase - y);
 
-		int x = MARGIN,y;
-
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-	
-		// TODO - SLUTT
+		}
+		setColor(0, 255, 0);
+		drawLine(MARGIN, ybase - avgSpeed, getWidth() - MARGIN, ybase - avgSpeed);
 	}
 }
